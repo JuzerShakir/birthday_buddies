5b183c13664f6e3268365d5f0250ba76366613b2
Juzer Shakir <juzershakir.webdev@gmail.com>
2024-07-02 10:57:14 +0530

    set `null` & `default` for `first_name` & `last_name` attributes

--- db/migrate/20240702045734_devise_create_users.rb
+++ db/migrate/20240702045734_devise_create_users.rb
@@ -32,8 +32,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.1]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
-      t.string :first_name
-      t.string :last_name
+      t.string :first_name, null: false, default: ""
+      t.string :last_name, null: false, default: ""
 
       t.timestamps null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -21,8 +21,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_02_045734) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
-    t.string "first_name"
-    t.string "last_name"
+    t.string "first_name", default: "", null: false
+    t.string "last_name", default: "", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
