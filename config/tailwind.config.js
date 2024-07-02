d3ece01984fc215ac311bfc30dd283d564a7942d
Juzer Shakir <juzershakir.webdev@gmail.com>
2024-07-02 11:44:38 +0530

    config default theme

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -6,13 +6,47 @@ module.exports = {
 		"./app/helpers/**/*.rb",
 		"./app/javascript/**/*.js",
 		"./app/views/**/*.{erb,haml,html,slim}",
+		"./app/assets/stylesheets/**/*.css",
 	],
+
 	theme: {
 		fontFamily: {
 			sans: ["Nunito Sans", ...defaultTheme.fontFamily.sans],
 		},
-		extend: {},
+		extend: {
+			colors: {
+				// Blue
+				primary: {
+					50: "#eff6ff",
+					100: "#dbeafe",
+					200: "#bfdbfe",
+					300: "#93c5fd",
+					400: "#60a5fa",
+					500: "#3b82f6",
+					600: "#2563eb",
+					700: "#1d4ed8",
+					800: "#1e40af",
+					900: "#1e3a8a",
+					950: "#172554",
+				},
+				// sky
+				secondary: {
+					50: "#f0f9ff",
+					100: "#e0f2fe",
+					200: "#bae6fd",
+					300: "#7dd3fc",
+					400: "#38bdf8",
+					500: "#0ea5e9",
+					600: "#0284c7",
+					700: "#0369a1",
+					800: "#075985",
+					900: "#0c4a6e",
+					950: "#082f49",
+				},
+			},
+		},
 	},
+
 	plugins: [
 		require("@tailwindcss/forms"),
 		require("@tailwindcss/typography"),
