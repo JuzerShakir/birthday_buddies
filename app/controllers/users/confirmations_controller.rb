e6262e40af912fd7cfe997b2509e09e6e34ec7a8
Juzer Shakir <juzershakir.webdev@gmail.com>
2024-07-03 11:25:40 +0530

    update `after_confirmation_path` to `birthday_buddies_path`

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -3,6 +3,6 @@ class Users::ConfirmationsController < Devise::ConfirmationsController
   # after confirming email address
   def after_confirmation_path_for(resource_name, resource)
     sign_in(resource) # sign in the user
-    # root_path
+    birthday_buddies_path
   end
 end
