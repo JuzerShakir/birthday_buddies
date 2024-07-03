eccb6bccbe0ca501f6cbcf5bb2cfaff8f928b85f
Juzer Shakir <juzershakir.webdev@gmail.com>
2024-07-03 13:29:48 +0530

    add birthday form layout & creating logic

--- app/controllers/birthday_buddies_controller.rb
+++ app/controllers/birthday_buddies_controller.rb
@@ -2,5 +2,22 @@ class BirthdayBuddiesController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @birthday_buddies = current_user.birthday_buddies
+    @birthday_buddy = current_user.birthday_buddies.new
+  end
+
+  def create
+    @birthday_buddy = current_user.birthday_buddies.new(birthday_buddy_params)
+    p birthday_buddy_params
+
+    if @birthday_buddy.save
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def birthday_buddy_params
+    params.require(:birthday_buddy).permit(:first_name, :last_name, :gregorian_birthday)
   end
 end
