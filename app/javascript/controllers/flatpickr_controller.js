58d2696483655e7bfeb34dae741d95d9254d2874
Juzer Shakir <juzershakir.webdev@gmail.com>
2024-07-03 15:28:43 +0530

    show in `mm-dd,yy` date format after re-rendering page

--- app/javascript/controllers/flatpickr_controller.js
+++ app/javascript/controllers/flatpickr_controller.js
@@ -5,7 +5,9 @@ import flatpickr from "flatpickr";
 export default class extends Controller {
 	connect() {
 		flatpickr(".gregorian-date", {
-			dateFormat: "F j, Y",
+			altInput: true,
+			altFormat: "F j, Y",
+			dateFormat: "Y-m-d",
 			maxDate: new Date(),
 		});
 	}
