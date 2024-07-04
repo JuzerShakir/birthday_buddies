5b828dfa78e03e092e37a42ad1448f947cb69a14
Juzer Shakir <juzershakir.webdev@gmail.com>
2024-07-04 14:44:34 +0530

    remove `:new` & `:show`  routes from `birthday_buddies`

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   devise_for :users, controllers: { confirmations: 'users/confirmations' }
 
-  resources :birthday_buddies
+  resources :birthday_buddies, except: [:new, :show]
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
