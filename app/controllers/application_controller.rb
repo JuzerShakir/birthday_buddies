34405f352f26140f33a0d29f354b870480c005a8
Juzer Shakir <juzershakir.webdev@gmail.com>
2024-07-04 17:38:15 +0530

    update edit form to show `name`

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
 
   def after_sign_in_path_for(resource)
