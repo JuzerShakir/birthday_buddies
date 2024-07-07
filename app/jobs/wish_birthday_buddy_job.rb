b8a2c2a9740653a5d98a5417dd12a8d47dd7763d
Juzer Shakir <juzershakir.webdev@gmail.com>
2024-07-07 20:33:48 +0530

    setup proper arguments to deliver email

--- app/jobs/wish_birthday_buddy_job.rb
+++ app/jobs/wish_birthday_buddy_job.rb
@@ -2,6 +2,7 @@ class WishBirthdayBuddyJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    HappyBirthdayMailer.with(user: User.first, birthday_buddy: User.first.birthday_buddies.first).wish_happy_birthday_email.deliver_now
+    user, birthday_buddy =  args
+    HappyBirthdayMailer.with(user: , birthday_buddy: ).wish_happy_birthday_email.deliver_later
   end
 end
