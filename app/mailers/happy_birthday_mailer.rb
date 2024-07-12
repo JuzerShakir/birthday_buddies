91da25bc0351b4c6c449e2a5ac92406211b1dabd
Juzer Shakir <juzershakir.webdev@gmail.com>
2024-07-12 18:51:07 +0530

    set mailer method & views for hijri birthday reminders

--- app/mailers/happy_birthday_mailer.rb
+++ app/mailers/happy_birthday_mailer.rb
@@ -5,4 +5,11 @@ class HappyBirthdayMailer < ApplicationMailer
 
     mail(to: email_address_with_name(@well_wisher.email, @well_wisher.name), subject: "It's #{@birthday_celebrant.name.possessive} birthday today!" )
   end
+
+  def wish_happy_hijri_birthday_email
+    @well_wisher = params[:user]
+    @birthday_celebrant = params[:birthday_buddy]
+
+    mail(to: email_address_with_name(@well_wisher.email, @well_wisher.name), subject: "It's #{@birthday_celebrant.name.possessive} waras today!" )
+  end
 end
