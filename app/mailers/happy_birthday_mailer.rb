4fac62ebf1fe23cc9d869a51af368ccde261b22b
Juzer Shakir <juzershakir.webdev@gmail.com>
2024-07-18 11:12:42 +0530

    use `before_action` to set instance vars for mailer methods

--- app/mailers/happy_birthday_mailer.rb
+++ app/mailers/happy_birthday_mailer.rb
@@ -1,15 +1,24 @@
 class HappyBirthdayMailer < ApplicationMailer
-  def wish_happy_gregorian_birthday_email
-    @well_wisher = params[:user]
-    @birthday_celebrant = params[:birthday_buddy]
+  before_action :set_well_wisher_and_birthday_celebrant
 
-    mail(to: email_address_with_name(@well_wisher.email, @well_wisher.name), subject: "It's #{@birthday_celebrant.name.possessive} birthday today!" )
+  def wish_happy_gregorian_birthday_email
+    mail(
+      to: email_address_with_name(@well_wisher.email, @well_wisher.name),
+      subject: "It's #{@birthday_celebrant.name.possessive} birthday today!"
+      )
   end
 
   def wish_happy_hijri_birthday_email
+    mail(
+      to: email_address_with_name(@well_wisher.email, @well_wisher.name),
+      subject: "It's #{@birthday_celebrant.name.possessive} waras today!"
+      )
+  end
+
+  private
+
+  def set_well_wisher_and_birthday_celebrant
     @well_wisher = params[:user]
     @birthday_celebrant = params[:birthday_buddy]
-
-    mail(to: email_address_with_name(@well_wisher.email, @well_wisher.name), subject: "It's #{@birthday_celebrant.name.possessive} waras today!" )
   end
 end
