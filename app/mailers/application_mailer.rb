8f2b1f53bca2fffbb7c57e959a1c6a441e7c9112
Juzer Shakir <juzershakir.webdev@gmail.com>
2024-07-26 11:30:49 +0530

    add space character in app name

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: email_address_with_name(Rails.application.credentials.dig(:mailer, :email), "BirthdayBuddies")
+  default from: email_address_with_name(Rails.application.credentials.dig(:mailer, :email), "Birthday Buddies")
   layout "mailer"
 end
